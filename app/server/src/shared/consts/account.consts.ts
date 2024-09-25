7209d747048687d99ff02837699e86cf1c26c0e9
pagoru <pagoru@gmail.com>
2024-09-25 19:47:05 +0200

    fix: password regex minimums - fix #57

--- app/server/src/shared/consts/account.consts.ts
+++ app/server/src/shared/consts/account.consts.ts
@@ -1,7 +1,6 @@
 export const USERNAME_REGEX = /^[a-zA-Z0-9_]{3,16}$/;
 
-export const PASSWORD_REGEX =
-  /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$/;
+export const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
 
 export const EMAIL_REGEX = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/;
 
