728220e6423fb99305f41982b309b1016667441f
pagoru <pagoru@gmail.com>
2024-10-24 15:48:17 +0200

    feat: add components library - fix #147

--- app/client/src/main.tsx
+++ app/client/src/main.tsx
@@ -3,10 +3,16 @@ import { createRoot } from "react-dom/client";
 
 // @ts-ignore
 import "./main.module.scss";
+import "@oh/styles/style.css";
 
+import { Application } from "@oh/components";
 import { ApplicationComponent } from "modules/application";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 
-root.render(<ApplicationComponent />);
+root.render(
+  <Application>
+    <ApplicationComponent />
+  </Application>,
+);
