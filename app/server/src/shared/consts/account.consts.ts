abd6917a14d0c5b97770760191ed7ce8e39fb9ff
cout970 <thecout970@gmail.com>
2024-12-01 18:09:26 +0100

    Update email and password regex at register
    New email regex, looks crazy but supports gmail alias and weird domains
    New password regex, owasp recommends no limits on char types

--- app/server/src/shared/consts/account.consts.ts
+++ app/server/src/shared/consts/account.consts.ts
@@ -1,5 +1,11 @@
 export const USERNAME_REGEX = /^[a-zA-Z0-9_]{3,16}$/;
 
-export const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
+// Passwords should be at least 8 characters long, allow any character and "There should be no requirement for
+// upper or lower case or numbers or special characters."
+// https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html#implement-proper-password-strength-controls
+export const PASSWORD_REGEX = /^.{8,}$/;
 
-export const EMAIL_REGEX = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/;
+// Regex is not the appropriate way to validate email addresses, but anyway...
+// https://stackoverflow.com/a/201378
+export const EMAIL_REGEX =
+  /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
