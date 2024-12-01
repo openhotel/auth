a597fef95ea40f604e9e086997160d219ba5611c
cout970 <thecout970@gmail.com>
2024-12-01 18:21:07 +0100

    Update register form client-side validation

--- app/client/src/modules/register/register.component.tsx
+++ app/client/src/modules/register/register.component.tsx
@@ -5,10 +5,16 @@ import {
   RedirectComponent,
 } from "shared/components";
 import { useAccount } from "shared/hooks";
-import styles from "./register.module.scss";
 import login_styles from "../login/login.module.scss";
 import { useNavigate } from "react-router-dom";
 import { ButtonComponent, InputComponent } from "@oh/components";
+import {
+  EMAIL_REGEX,
+  PASSWORD_MIN_LEN,
+  USERNAME_MAX_LEN,
+  USERNAME_MIN_LEN,
+  USERNAME_VALID_CHARS,
+} from "../../shared/consts/account.consts";
 
 export const RegisterComponent: React.FC = () => {
   const [submittedAt, setSubmittedAt] = useState<number>();
@@ -48,27 +54,146 @@ export const RegisterComponent: React.FC = () => {
     <div className={login_styles.wrapper}>
       <form className={login_styles.form} onSubmit={onSubmit}>
         <h1 className={login_styles.title}>Register</h1>
-        <InputComponent name="email" placeholder="Email" />
-        <InputComponent name="username" placeholder="Username" />
-        <InputComponent
-          name="password"
-          placeholder="Password"
-          type="password"
-        />
-        <InputComponent
-          name="rePassword"
-          placeholder="Repeat password"
-          type="password"
-        />
+        <EmailComponent />
+        <UsernameComponent />
+        <PasswordComponent />
         <CaptchaComponent submittedAt={submittedAt} onResolve={setCaptchaId} />
         <ButtonComponent fullWidth>Register</ButtonComponent>
         {errorMessage ? (
-          <label className={login_styles.error}>{errorMessage}</label>
+          <label key="backend-error" className={login_styles.error}>
+            {errorMessage}
+          </label>
         ) : null}
       </form>
       <LinkComponent className={login_styles.link} to="/login">
         Already registered? Login here.
       </LinkComponent>
     </div>
+  );
+};
+
+const EmailComponent: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [invalid, setInvalid] = useState(false);
+
+  const check = () => {
+    if (email === "") {
+      setInvalid(false);
+      return;
+    }
+    setInvalid(!new RegExp(EMAIL_REGEX).test(email));
+  };
+
+  return (
+    <>
+      <InputComponent
+        name="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail((e.target as HTMLInputElement).value)}
+        onBlur={check}
+      />
+
+      {invalid ? (
+        <label key="error-invalid" className={login_styles.error}>
+          Invalid email
+        </label>
+      ) : null}
+    </>
+  );
+};
+
+const UsernameComponent: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [tooShort, setTooShort] = useState(false);
+  const [invalidChars, setInvalidChars] = useState(false);
+  const [tooLong, setTooLong] = useState(false);
+
+  const check = () => {
+    if (username === "") {
+      setTooShort(false);
+      setTooLong(false);
+      setInvalidChars(false);
+      return;
+    }
+    setTooShort(username.length < USERNAME_MIN_LEN);
+    setTooLong(username.length > USERNAME_MAX_LEN);
+    console.log(username, new RegExp(USERNAME_VALID_CHARS).test(username));
+    setInvalidChars(!new RegExp(USERNAME_VALID_CHARS).test(username));
+  };
+
+  return (
+    <>
+      <InputComponent
+        name="username"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername((e.target as HTMLInputElement).value)}
+        onBlur={check}
+      />
+
+      {tooShort ? (
+        <label key="error-short" className={login_styles.error}>
+          Username is too short
+        </label>
+      ) : null}
+
+      {tooLong ? (
+        <label key="error-long" className={login_styles.error}>
+          Username is too long
+        </label>
+      ) : null}
+
+      {invalidChars ? (
+        <label key="error-invalid" className={login_styles.error}>
+          Username contains invalid characters
+        </label>
+      ) : null}
+    </>
+  );
+};
+
+const PasswordComponent: React.FC = () => {
+  const [pass1, setPass1] = useState("");
+  const [pass2, setPass2] = useState("");
+  const [passMatch, setPassMatch] = useState(true);
+  const [passTooShort, setPassTooShort] = useState(false);
+
+  const check = () => {
+    setPassMatch(pass1 === pass2 || pass2 === "");
+    setPassTooShort(pass1 && pass1.length < PASSWORD_MIN_LEN);
+  };
+
+  return (
+    <>
+      <InputComponent
+        name="password"
+        placeholder="Password"
+        type="password"
+        value={pass1}
+        onChange={(e) => setPass1((e.target as HTMLInputElement).value)}
+        onBlur={check}
+      />
+      <InputComponent
+        name="rePassword"
+        placeholder="Repeat password"
+        type="password"
+        value={pass2}
+        onChange={(e) => setPass2((e.target as HTMLInputElement).value)}
+        onBlur={check}
+      />
+
+      {!passMatch ? (
+        <label key="error-match" className={login_styles.error}>
+          Passwords don't match
+        </label>
+      ) : null}
+
+      {passTooShort ? (
+        <label key="error-short" className={login_styles.error}>
+          Password is too short
+        </label>
+      ) : null}
+    </>
   );
 };
---
+++ app/client/src/shared/consts/account.consts.ts
@@ -0,0 +1,11 @@
+export const USERNAME_MIN_LEN = 3;
+export const USERNAME_MAX_LEN = 16;
+
+export const USERNAME_VALID_CHARS = /^[a-zA-Z0-9_]+$/;
+
+export const PASSWORD_MIN_LEN = 8;
+
+// Regex is not the appropriate way to validate email addresses, but anyway...
+// https://stackoverflow.com/a/201378
+export const EMAIL_REGEX =
+  /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
