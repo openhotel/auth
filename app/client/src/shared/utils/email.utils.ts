284f92fa3a345980c87f39b89a71470aa9727176
pagoru <pagoru@gmail.com>
2025-01-03 18:18:56 +0100

    feat: improve user admin table - fix #235

--- app/client/src/shared/utils/email.utils.ts
+++ app/client/src/shared/utils/email.utils.ts
@@ -1,4 +1,9 @@
+import { EMAIL_REGEX } from "shared/consts";
+
 export const getCensoredEmail = (email: string): string =>
   email.substring(0, 1) +
   "**@**.**" +
   email.substring(email.length - 1, email.length);
+
+export const isEmail = (email: string): boolean =>
+  new RegExp(EMAIL_REGEX).test(email);
