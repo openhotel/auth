da1a300e38a00b034352b6044b4707cfb3a45736
Pablo González Rubio <pagoru@gmail.com>
2025-01-22 13:30:25 +0100

    fix: filter out null hotels - fix #253 (#254)

--- app/server/src/modules/api/v3/hotel/list.request.ts
+++ app/server/src/modules/api/v3/hotel/list.request.ts
@@ -14,60 +14,61 @@ export const listRequest: RequestType = {
   func: async () => {
     const $hotels = await System.hotels.getList();
 
-    const hotels = await Promise.all(
-      $hotels
-        .filter((hotel) => hotel.public)
-        .map(async (hotel) => {
-          const owner = await System.accounts.get(hotel.accountId);
-          const client = hotel.integrations.find(
-            (integration) => integration.type === "client",
-          );
-          const web = hotel.integrations.find(
-            (integration) => integration.type === "web",
-          );
+    const hotels = (
+      await Promise.all(
+        $hotels
+          .filter((hotel) => hotel.public)
+          .map(async (hotel) => {
+            const owner = await System.accounts.get(hotel.accountId);
+            const client = hotel.integrations.find(
+              (integration) => integration.type === "client",
+            );
+            const web = hotel.integrations.find(
+              (integration) => integration.type === "web",
+            );
 
-          const clientAccounts = client
-            ? (
-                await System.hotels.getAccountsByIntegrationId(
-                  hotel.hotelId,
-                  client.integrationId,
-                )
-              ).length
-            : 0;
-          const webAccounts = web
-            ? (
-                await System.hotels.getAccountsByIntegrationId(
-                  hotel.hotelId,
-                  web.integrationId,
-                )
-              ).length
-            : 0;
+            const clientAccounts = client
+              ? (
+                  await System.hotels.getAccountsByIntegrationId(
+                    hotel.hotelId,
+                    client.integrationId,
+                  )
+                ).length
+              : 0;
+            const webAccounts = web
+              ? (
+                  await System.hotels.getAccountsByIntegrationId(
+                    hotel.hotelId,
+                    web.integrationId,
+                  )
+                ).length
+              : 0;
 
-          //if hotel is a ghost town, filter it out
-          if ((!client && !web) || clientAccounts + webAccounts === 0)
-            return null;
+            //if hotel is a ghost town, filter it out
+            if ((!client && !web) || clientAccounts + webAccounts === 0)
+              return null;
 
-          return {
-            name: hotel.name,
-            owner: owner.username,
-            client: client
-              ? {
-                  name: client.name,
-                  url: client.redirectUrl,
-                  accounts: clientAccounts,
-                }
-              : null,
-            web: web
-              ? {
-                  name: web.name,
-                  url: web.redirectUrl,
-                  accounts: webAccounts,
-                }
-              : null,
-          };
-        })
-        .filter(Boolean),
-    );
+            return {
+              name: hotel.name,
+              owner: owner.username,
+              client: client
+                ? {
+                    name: client.name,
+                    url: client.redirectUrl,
+                    accounts: clientAccounts,
+                  }
+                : null,
+              web: web
+                ? {
+                    name: web.name,
+                    url: web.redirectUrl,
+                    accounts: webAccounts,
+                  }
+                : null,
+            };
+          }),
+      )
+    ).filter(Boolean);
     return getResponse(HttpStatusCode.OK, {
       data: {
         hotels,
