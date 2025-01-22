50ac2a2d70d5eb1a4ff471da3ff74ea32fd1798f
Pablo González Rubio <pagoru@gmail.com>
2025-01-22 13:20:07 +0100

    feat: remove last hotel button - fix #250 (#251)

--- app/client/src/modules/home/components/navigator/navigator.component.tsx
+++ app/client/src/modules/home/components/navigator/navigator.component.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { useRedirect } from "shared/hooks";
-import { HotelIconComponent, NavItemComponent } from "@oh/components";
 
 export const HomeNavigatorComponent: React.FC = () => {
-  const { navigate } = useRedirect();
+  // const { navigate } = useRedirect();
 
   return (
     <>
-      <NavItemComponent onClick={navigate} icon={<HotelIconComponent />}>
-        Last Hotel
-      </NavItemComponent>
+      {/*<NavItemComponent onClick={navigate} icon={<HotelIconComponent />}>*/}
+      {/*  Last Hotel*/}
+      {/*</NavItemComponent>*/}
     </>
   );
 };
