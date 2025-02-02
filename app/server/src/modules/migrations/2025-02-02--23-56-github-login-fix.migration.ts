06621e9241fd3bce442e0034a7f82efdfc47db36
Pablo González Rubio <pagoru@gmail.com>
2025-02-03 00:04:30 +0100

    fix: github login incorrect - fix #299 (#300)

---
+++ app/server/src/modules/migrations/2025-02-02--23-56-github-login-fix.migration.ts
@@ -0,0 +1,36 @@
+import { Migration, DbMutable } from "@oh/utils";
+
+export default {
+  id: "2025-02-02--23-56-github-login-fix",
+  description: `fix github login`,
+  up: async (db: DbMutable) => {
+    const accounts = await db.list({
+      prefix: ["accounts"],
+    });
+
+    for await (const { key, value } of accounts) {
+      if (!value.githubLogin) continue;
+
+      await db.set(key, {
+        ...value,
+        githubLogin: value.githubLogin.login,
+        updatedAt: Date.now(),
+      });
+    }
+  },
+  down: async (db: DbMutable) => {
+    const accounts = await db.list({
+      prefix: ["accounts"],
+    });
+
+    for await (const { key, value } of accounts) {
+      if (!value.githubLogin) continue;
+
+      await db.set(key, {
+        ...value,
+        githubLogin: { login: value.githubLogin.login },
+        updatedAt: Date.now(),
+      });
+    }
+  },
+} as Migration;
