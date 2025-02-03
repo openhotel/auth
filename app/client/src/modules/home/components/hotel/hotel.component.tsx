1dfa827d738da1db8e11cd98aa23c97b2ed2138f
Pablo González Rubio <pagoru@gmail.com>
2025-02-03 23:47:55 +0100

    feat: implement hotel card component - fix #308 (#309)

--- app/client/src/modules/home/components/hotel/hotel.component.tsx
+++ app/client/src/modules/home/components/hotel/hotel.component.tsx
@@ -1,122 +1,62 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import dayjs from "dayjs";
-import { LinkComponent } from "shared/components";
-import { ButtonComponent } from "@oh/components";
+import React, { useCallback, useEffect, useState } from "react";
+import { HotelCardComponent } from "@oh/components";
 import { PublicHotel } from "shared/types";
-//@ts-ignore
-import styles from "./hotel.module.scss";
-import { PingHotelStatus } from "shared/enums";
-import { cn } from "shared/utils";
 
 type Props = {
   hotel: PublicHotel;
 };
 
 export const HotelComponent: React.FC<Props> = ({ hotel }) => {
-  const [pingClientStatus, setPingClientStatus] = useState<PingHotelStatus>(
-    PingHotelStatus.LOADING,
-  );
-  const [clientVersion, setClientVersion] = useState<string>(null);
+  const [pingMs, setPingMs] = useState<number>(null);
+  const [clientVersion, setClientVersion] = useState<string>(undefined);
 
   const $ping = useCallback(() => {
     const pingUrl = new URL(hotel.client.url);
     pingUrl.pathname = "version";
+
+    let initialDate = Date.now();
     fetch(pingUrl.href)
       .then((response) => response.json())
       .then(({ status, data }) => {
-        if (status !== 200)
-          return setPingClientStatus(PingHotelStatus.NOT_REACHED);
+        if (status !== 200) return setPingMs(undefined);
 
         setClientVersion(data.version);
-        setPingClientStatus(PingHotelStatus.REACHED);
+        setPingMs(Date.now() - initialDate);
       })
       .catch(() => {
-        setPingClientStatus(PingHotelStatus.NOT_REACHED);
+        setPingMs(undefined);
       });
-  }, [hotel, setPingClientStatus, setClientVersion]);
+  }, [hotel, setPingMs, setClientVersion]);
 
   useEffect(() => {
-    if (!hotel.client) {
-      setPingClientStatus(PingHotelStatus.NOT_REACHED);
-      return;
-    }
-
     $ping();
 
     const interval = setInterval($ping, 60_000);
     return () => clearInterval(interval);
   }, [$ping]);
 
-  const hasNotReached = useMemo(
-    () => pingClientStatus === PingHotelStatus.NOT_REACHED,
-    [pingClientStatus],
-  );
-  const hasReached = useMemo(
-    () => pingClientStatus === PingHotelStatus.REACHED,
-    [pingClientStatus],
-  );
+  const onClickWebsite = useCallback(() => {
+    open(hotel.web.url, "_blank");
+  }, []);
+  const onClickClient = useCallback(() => {
+    open(hotel.client.url, "_blank");
+  }, []);
 
   return (
-    <div className={styles.hotel} key={hotel.id}>
-      <div
-        className={cn(styles.header, {
-          [styles.notReached]: hasNotReached,
-        })}
-        style={{
-          backgroundImage: `url(hotel/hotel-background.webp)`,
-        }}
-      >
-        <div
-          className={styles.logo}
-          style={{
-            backgroundImage: `url(hotel/hotel-logo.webp)`,
-          }}
-        />
-        <div className={styles.gradient} />
-      </div>
-      <div className={styles.content}>
-        <div className={styles.contentHeader}>
-          {hasNotReached ? <b className={styles.offline}>OFFLINE</b> : null}
-          {hasReached ? (
-            <b className={styles.online}>ONLINE ({clientVersion})</b>
-          ) : null}
-          <span>
-            <b>{hotel.name} </b>
-            <a className={styles.owner}>by {hotel.owner}</a>
-          </span>
-          <label>
-            founded on {dayjs(hotel.createdAt).format("DD MMM YYYY")}
-          </label>
-          <a>
-            {hotel.accounts} user{hotel.accounts === 1 ? "" : "s"} already
-            joined!
-          </a>
-        </div>
-        <div className={styles.contentActions}>
-          {hasReached ? (
-            <>
-              {hotel.web ? (
-                <div className={styles.action}>
-                  <LinkComponent to={hotel.web.url} target="_blank">
-                    <ButtonComponent variant="3d">
-                      Visit the website!
-                    </ButtonComponent>
-                  </LinkComponent>
-                </div>
-              ) : null}
-              {hotel.client ? (
-                <div className={styles.action}>
-                  <LinkComponent to={hotel.client.url} target="_blank">
-                    <ButtonComponent color="yellow" variant="3d">
-                      Check in!
-                    </ButtonComponent>
-                  </LinkComponent>
-                </div>
-              ) : null}
-            </>
-          ) : null}
-        </div>
-      </div>
-    </div>
+    <HotelCardComponent
+      official={false}
+      verified={false}
+      owner={hotel.owner}
+      title={hotel.name}
+      description={"This is a default description"}
+      logo={"/assets/hotel/hotel-logo.webp"}
+      background={"/assets/hotel/hotel-background.webp"}
+      onClickWebsite={hotel.web?.url ? onClickWebsite : undefined}
+      onClickClient={onClickClient}
+      ms={pingMs}
+      joinedUsers={hotel.accounts}
+      createdAt={hotel.createdAt}
+      version={clientVersion}
+    />
   );
 };
