88dbe509ec61a2a16b268e0e65b6d932023e10b9
pagoru <pagoru@gmail.com>
2025-02-20 13:27:33 +0100

    feat: improved db s3 backups and crypto - fix #358

--- app/server/src/shared/utils/password.utils.ts
+++ app/server/src/shared/utils/password.utils.ts
@@ -4,5 +4,5 @@ import { System } from "modules/system/main.ts";
 export const getPasswordMaxLength = async (): Promise<number> => {
   const pepper = await System.db.crypto.pepperPassword("");
 
-  return BCRYPT_MAX_LENGTH - pepper.length - 1;
+  return BCRYPT_MAX_LENGTH - pepper.length;
 };
