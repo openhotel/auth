51f66223aa0b7c2eca21d7413f5e03535f4cd4de
Pablo González Rubio <pagoru@gmail.com>
2025-03-22 23:03:16 +0100

    fix: invalid email and prevent lengthy ones - fix #403 (#406)

--- app/server/src/shared/utils/mail.utils.ts
+++ app/server/src/shared/utils/mail.utils.ts
@@ -1,2 +1,6 @@
+import { BCRYPT_MAX_LENGTH } from "shared/consts/password.consts.ts";
+
 export const getHiddenMail = (mail: string) =>
   `${mail.substring(0, 3)}***${mail.substring(mail.length - 3)}`;
+
+export const getEmailMaxLength = () => BCRYPT_MAX_LENGTH;
