ce93bdb12453dbe0d8659ab87d0e81c9297845af
pagoru <pagoru@gmail.com>
2025-06-05 12:20:02 +0200

    feat: added daily analytics - fix #434

--- app/client/src/modules/admin/components/analytics/analytics.component.tsx
+++ app/client/src/modules/admin/components/analytics/analytics.component.tsx
@@ -1,56 +1,10 @@
-import React, { useEffect, useMemo, useState } from "react";
-import {
-  VictoryAxis,
-  VictoryChart,
-  VictoryGroup,
-  VictoryLegend,
-  VictoryLine,
-  VictoryTheme,
-} from "victory";
-
-//@ts-ignore
-import styles from "./analytics.module.scss";
+import React, { useEffect, useState } from "react";
 import { useAdmin } from "shared/hooks";
 import dayjs from "dayjs";
-
-const CustomTickLabel = ({ x, y, text, ...rest }) => (
-  <g transform={`translate(${x},${y})`}>
-    <text
-      transform="rotate(-30)"
-      textAnchor="end"
-      dy="0.3em"
-      style={{ fontSize: 6, fill: "currentColor" }}
-    >
-      {text}
-    </text>
-  </g>
-);
-
-const CUSTOM_DARK_THEME = {
-  ...VictoryTheme.grayscale, // Start with the grayscale theme
-  axis: {
-    ...VictoryTheme.grayscale.axis,
-    style: {
-      ...VictoryTheme.grayscale.axis.style,
-      axis: {
-        ...VictoryTheme.grayscale.axis.style.axis,
-        stroke: "currentColor", // White axis lines
-      },
-      tickLabels: {
-        ...VictoryTheme.grayscale.axis.style.tickLabels,
-        fill: "currentColor", // White tick labels
-      },
-      grid: {
-        ...VictoryTheme.grayscale.axis.style.grid,
-        stroke: "currentColor", // Darker grid lines
-        opacity: 0.125,
-      },
-    },
-  },
-} as typeof VictoryTheme.grayscale;
+import { DailyChartComponent, TimelineChartComponent } from "./components";
 
 export const AnalyticsComponent: React.FC = () => {
-  const { users, hotels, fetchUsers, fetchHotels } = useAdmin();
+  const { users, fetchUsers, fetchHotels } = useAdmin();
 
   useEffect(() => {
     fetchUsers();
@@ -60,9 +14,6 @@ export const AnalyticsComponent: React.FC = () => {
   const [startDate, setStartDate] = useState(dayjs());
 
   const [dateRange, setDateRange] = useState([]);
-
-  const [usersData, setUsersData] = useState([]);
-  const [hotelsData, setHotelsData] = useState([]);
 
   useEffect(() => {
     if (!users.length) return;
@@ -78,128 +29,19 @@ export const AnalyticsComponent: React.FC = () => {
     setStartDate(startDate);
 
     const dateRange = [];
-    for (let i = 0; i <= dayjs().diff(startDate, "day"); i++)
+    for (let i = 0; i <= dayjs().add(1, "day").diff(startDate, "day"); i++)
       dateRange.push(startDate.add(i, "day").format("YYYY/MM/DD"));
 
     setDateRange(dateRange);
-    setUsersData(
-      dateRange.reduce(
-        (data, day, index) => [
-          ...data,
-          (data[index - 1] ?? 0) +
-            usersSorted.filter((user) => day === user._createdAt).length,
-        ],
-        [],
-      ),
-    );
-  }, [users, setDateRange, setUsersData, setStartDate]);
-
-  useEffect(() => {
-    if (!hotels.length || !dateRange.length) return;
-
-    setHotelsData(
-      dateRange.reduce(
-        (data, day, index) => [
-          ...data,
-          (data[index - 1] ?? 0) +
-            hotels.filter(
-              (hotel) => day === dayjs(hotel.createdAt).format("YYYY/MM/DD"),
-            ).length,
-        ],
-        [],
-      ),
-    );
-  }, [hotels, dateRange, setHotelsData]);
-
-  const series = useMemo(
-    () => [
-      {
-        name: "Users",
-        data: usersData,
-      },
-      {
-        name: "Hotels",
-        data: hotelsData,
-      },
-    ],
-    [usersData, hotelsData],
-  );
-
-  const domain = useMemo(
-    () => ({
-      y: [0, Math.max(...series.map((serie) => serie.data.pop())) + 50],
-    }),
-    [series],
-  );
+  }, [users, setDateRange, setStartDate]);
 
   return (
-    <div className={styles.analytics}>
+    <div>
       <h2>Analytics</h2>
-      <VictoryChart
-        theme={CUSTOM_DARK_THEME}
-        //@ts-ignore
-        domain={domain}
-        height={200}
-      >
-        <VictoryAxis
-          tickValues={dateRange}
-          tickFormat={(date) => {
-            return dayjs(date).date() === 1 ||
-              date === startDate.format("YYYY/MM/DD") ||
-              date === dayjs().format("YYYY/MM/DD")
-              ? date
-              : null;
-          }}
-          //@ts-ignore
-          tickLabelComponent={<CustomTickLabel />}
-        />
-        {/*@ts-ignore*/}
-        <VictoryAxis
-          dependentAxis
-          style={{
-            tickLabels: {
-              fontSize: 6,
-            },
-            grid: {
-              stroke: "currentColor",
-              size: 5,
-            },
-          }}
-        />
-        {series.map((serie, i) => (
-          <VictoryGroup data={serie.data} key={serie.name}>
-            <VictoryLine
-              style={{
-                data: {
-                  stroke: VictoryTheme.clean.palette.qualitative[i],
-                  strokeWidth: 1,
-                },
-              }}
-            />
-          </VictoryGroup>
-        ))}
-        <VictoryLegend
-          x={20}
-          y={190}
-          orientation="horizontal"
-          data={[
-            {
-              id: "Users",
-              name: `Users (${users.length})`,
-              symbol: { fill: VictoryTheme.clean.palette.colors.blue },
-            },
-            {
-              id: "Hotels",
-              name: `Hotels (${hotels.length})`,
-              symbol: { fill: VictoryTheme.clean.palette.colors.cyan },
-            },
-          ]}
-          style={{
-            labels: { fill: "currentColor", fontSize: 6 },
-          }}
-          standalone={false}
-        />
-      </VictoryChart>
+      <h4>Timeline</h4>
+      <TimelineChartComponent startDate={startDate} dateRange={dateRange} />
+      <h4>Daily</h4>
+      <DailyChartComponent startDate={startDate} dateRange={dateRange} />
     </div>
   );
 };
---
+++ app/client/src/modules/admin/components/analytics/components/timeline-chart/index.ts
@@ -0,0 +1,2 @@
+
+export * from './timeline-chart.component'
