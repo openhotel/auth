d04bfa9d8e2d62688c08f37efcb3a1ba2582c589
pagoru <pagoru@gmail.com>
2026-04-04 23:20:10 +0200

    fix: update captcha to v2 - fix #451

--- app/client/src/modules/application/components/providers/providers.component.tsx
+++ app/client/src/modules/application/components/providers/providers.component.tsx
@@ -1,18 +1,16 @@
 import { Outlet } from "react-router";
-import { CaptchaV2Provider, TitleProvider, UserProvider } from "shared/hooks";
+import { TitleProvider, UserProvider } from "shared/hooks";
 import React from "react";
 import { ModalProvider } from "@openhotel/web-components";
 
 export const ProvidersComponent = () => {
   return (
-    <CaptchaV2Provider>
-      <TitleProvider>
-        <ModalProvider>
-          <UserProvider>
-            <Outlet />
-          </UserProvider>
-        </ModalProvider>
-      </TitleProvider>
-    </CaptchaV2Provider>
+    <TitleProvider>
+      <ModalProvider>
+        <UserProvider>
+          <Outlet />
+        </UserProvider>
+      </ModalProvider>
+    </TitleProvider>
   );
 };
